820000955d8e8c5fac52a1915f556a22a2945198
Iulian Curelea <iulian.curelea@itperspectives.ro>
2022-08-11 13:28:21 +0000

    Merged PR 9290: initial-setup+controller
    Related work items: #30209

---
+++ ITPLibrary.Api.Data/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITPLibrary.Api.Data.Entities
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ ITPLibrary.Api/Controllers/BookController.cs
@@ -0,0 +1,18 @@
+﻿using ITPLibrary.Api.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITPLibrary.Api.Controllers
+{
+    public class BookController : ControllerBase
+    {
+        public BookController()
+        {
+
+        }
+
+        public IEnumerable<Book> GetPopularBooks()
+        {
+            return new List<Book> { new Book() { Id = 0, Name = "title1", Author = "author1", Price = 200 }, new Book() { Id = 1, Name = "title2", Author = "author2", Price = 250 }, new Book() { Id = 2, Name = "title3", Author = "author4", Price = 300 } };
+        }
+    }
+}
