2230a5dd11b364ff01e971570b65296960e11dd7
Iulian Curelea <iulian.curelea@itperspectives.ro>
2022-09-20 11:48:41 +0300

    UpdateOrderDto

---
+++ ITPLibrary.Api.Core/Dtos/UpdateOrderDto.cs
@@ -0,0 +1,21 @@
+﻿
+
+using ITPLibrary.Api.Data.Entities;
+using ITPLibrary.Api.Data.Entities.ValidationRules;
+using System.ComponentModel.DataAnnotations;
+
+namespace ITPLibrary.Api.Core.Dtos
+{
+    public class UpdateOrderDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public Address BillingAddress { get; set; }
+
+        public Address DeliveryAddress { get; set; }
+
+        [MaxLength(OrderValidationRules.ObservationsMaxLength)]
+        public string Observations { get; set; }
+    }
+}
